5721950cf4104bb17762a965a5c253f6412a9b9d
Lutka <plutka88@gmail.com>
2014-11-07 22:32:12 +0000

    modify save, save one reading per file, add postfix to file name representing a sensorType, eg.T, H

--- test.c
+++ test.c
@@ -25,83 +25,115 @@
 #define FOLDER_NAME "readings"
 
 #define DEVICE_ID "1"
-#define URL_DESTINATION "http://weather.cs.nuim.ie/input.php"
+#define TEMP_TYPE "T"
+#define HUMID_TYPE "H"
+#define DESTINATION_URL "http://weather.cs.nuim.ie/input.php"
 
- int getTimeStamp()
- {
-      struct timeval timeVal;
-      gettimeofday(&timeVal,NULL);
-      printf(", Timestamp: %d\n", timeVal.tv_sec);
-      int time = ("%d\n", timeVal.tv_sec);
-      return time;
- }
+int getTimeStamp()
+{
+	struct timeval timeVal;
+	gettimeofday(&timeVal,NULL);
+	printf(", Timestamp: %d\n", timeVal.tv_sec);
+	int time = ("%d\n", timeVal.tv_sec);
+	return time;
+}
 
-    //create file and write reading values to it
- void writeToFile(int temp, int humidity)
- {
-     
-     struct stat st = {0};
-     
-     if(stat(FOLDER_NAME, &st)== -1)
-     {
-	mkdir(FOLDER_NAME , 0777);
-     }
+//create file and write reading values to it
+void writeToFile(int temp, int humidity)
+{
 
-     int timeStamp = getTimeStamp();
+	int timeStamp = getTimeStamp();
+	int paramsLen = 150; 
 
-     char fileName[23];
-     sprintf(fileName, "%s/%d.txt", FOLDER_NAME, timeStamp);
-    // printf(fileName);
-     
-     FILE *f = fopen(fileName, "w");
-     if(f == NULL)
-     {
-        printf("Error opening file!\n");
-        exit(1);
-     }
+	int numOfSensors = 2;
+	char sensorType[]={'T','H'};
+	char readingsParams[numOfSensors][paramsLen];
+	
 
-     fprintf(f, "%d\ ", timeStamp);
-     fprintf(f, "%d\ ", temp);
-     fprintf(f, "%d\ ", humidity);
+	//char tempParams [paramsLen];
+	sprintf(readingsParams[0], "device_id=%s&time=%d&value=%d&sensor_type=%s\n" , DEVICE_ID, timeStamp, temp, TEMP_TYPE);
+	
+	printf(readingsParams[0]);
+	
+	
+	//char humidityParams [paramsLen];
+	sprintf(readingsParams[1] , "device_id=%s&time=%d&value=%d&sensor_type=%s\n" , DEVICE_ID, timeStamp, humidity, HUMID_TYPE);
 
-     fclose(f);
- }
+	printf(readingsParams[1]);
+	
+	struct stat st = {0};
+	
+	if(stat(FOLDER_NAME, &st)== -1)
+	{
+		mkdir(FOLDER_NAME , 0777);
+	}
 
+	char fileName[25];
+
+	sprintf(fileName, "%s/%d.txt", FOLDER_NAME, timeStamp);
+	printf("Standard name: %s\n", fileName);
+	
+	
+	int i;
+	for(i = 0; i < numOfSensors; i++)
+	{
+		sprintf(fileName, "%s/%d_%c.txt", FOLDER_NAME, timeStamp, sensorType[i]);
+		
+		//printf("Name with sensor char: %s\n", fileName);
+
+		FILE *f = fopen(fileName, "w");
+		
+		fputs(readingsParams[i],f);
+
+		if(f == NULL)
+		{
+			printf("Error opening file!\n");
+			exit(1);
+		}
+
+
+		// fputs(tempParams, f);
+		//fputs(humidityParams, f);
+
+		// fflush(f);
+		fclose(f);
+	}
+}
 
 int main(int argc, char *argv[])
 {
 
-   int temp, humid;
-   int sensorType=DHT11;
-   int pin=4; // BCM(pin 4)=BOARD(pin 7)
-   float humidity = 0, temperature = 0;
-   int tries=10; // try up to 10 times
+	int temp, humid;
+	int sensorType=DHT11;
+	int pin=4; // BCM(pin 4)=BOARD(pin 7)
+	float humidity = 0, temperature = 0;
+	int tries=10; // try up to 10 times
 
-   // check if elevated
-   //getenv - gets value of an environmental varible
-   char *envar = getenv("SUDO_COMMAND");
-   if (envar == NULL) {
-      printf("Run with SUDO\n");
-   }
+	// check if elevated
+	//getenv - gets value of an environmental varible
+	char *envar = getenv("SUDO_COMMAND");
+	if (envar == NULL) {
+		printf("Run with SUDO\n");
+	}
 
-   else {
-      int result = DHT_ERROR_CHECKSUM;
-      while ((tries>0)&&(result!=DHT_SUCCESS)) {
-         tries--;
-         // get the sensor reading
-         result = pi_dht_read(sensorType, pin, &humidity, &temperature);
-      }
-      if (result==DHT_SUCCESS)
-	{
-	 printf("Temp=%6.2f°, humidity=%7.2f%% ", temperature, humidity);
-		
-	 temp = ("%6.2f", temperature);
-	 humid = ("%7.2f%%", humidity);
-	
-	writeToFile(temp, humid);
-	}     
-      else
-         printf("FAILURE: unable to read sensor\n");
-   }
+	else {
+		int result = DHT_ERROR_CHECKSUM;
+		while ((tries>0)&&(result!=DHT_SUCCESS)) {
+			tries--;
+			// get the sensor reading
+			result = pi_dht_read(sensorType, pin, &humidity, &temperature);
+		}
+		if (result==DHT_SUCCESS)
+		{
+			printf("Temp=%6.2f°, humidity=%7.2f%% ", temperature, humidity);
+			
+			temp = ("%6.2f", temperature);
+			humid = ("%7.2f%%", humidity);
+			
+			writeToFile(temp, humid);
+		}     
+		else
+		printf("FAILURE: unable to read sensor\n");
+	}
 
 }
