8c2b4c9bb1db2c26acb0abee75b6520db8958dea
Lutka <plutka88@gmail.com>
2014-11-22 14:41:54 +0000

    initial commit of code which gets values from gps

---
+++ gpsData.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+
+#include "gpsData.h"
+
+//char gpsLine[128];
+char line [ 128 ];
+
+//gps data values
+void findGpgga(char fileName[])
+{   
+   FILE *file = fopen ( fileName, "r" );
+   char gpsValues[] = "$GPGGA";
+   int gpsLinePresent = 1;
+   int i;
+   if ( file != NULL )
+   {
+      //char line [ 128 ]; /* or other suitable maximum line size */
+      while ( fgets ( line, sizeof line, file ) != NULL ) /* read a line */
+      {         
+		 i = 0;
+		 for(i; i < 6; ++i)
+		 {
+			if(gpsValues[i] != line[i])
+			{
+				gpsLinePresent = 0;
+				break;
+			}		
+		 }
+		 if(gpsLinePresent)
+		 {
+			return;
+		 }				 
+      }
+      fclose ( file );
+   }  
+   else
+   {
+      perror ( fileName ); /* why was unable to open the file*/
+   }
+ }
+
+int main ( void ) 
+{
+//tu jest code do odkodowanie danych z gps
+//http://www.boondog.com/tutorials/gps/gps.html
+   char fileName[] = "gpsReadings.txt";
+   char gpsLine[128];
+   findGpgga(fileName);
+   
+   fputs("GPS DATA LINE: ", stdout);
+   fputs(line, stdout);
+   char gps [14][12];
+   int index = 0;
+   char lat[10];
+   char lng[10];
+   char altitude[10];
+   
+   char* buffer;
+
+	buffer = (char *)strtok (line, ",");
+
+	while (buffer) 
+	{	
+		sprintf(gps[index], "%s" , buffer);		
+		printf ("Index: %i - %s\n", index, (char*)gps[index]);          // process token
+		buffer = (char *)strtok (NULL, ",");
+		++index;
+		while (buffer && *buffer == '\040')
+		{
+			buffer++;
+		}
+	}
+	sprintf(lat, "%s%s", gps[2], gps[3]);
+	sprintf(lng, "%s%s", gps[4], gps[5]);
+	sprintf(altitude, "%s", gps[9]);
+	
+	printf("%s\n", lat);
+	printf("%s\n", lng);
+	printf("%s\n", altitude); 
+   return 0;   
+}
